e4815e787ed02b2561e5cee07e51a2061303aa18
mitochon9 <mitochon9@gmail.com>
2022-01-30 16:30:52 +0900

    💄 minor change style

--- src/pages/index/About.tsx
+++ src/pages/index/About.tsx
@@ -45,20 +45,24 @@ export const About: VFC = () => {
       </h2>
       <h3 className="px-2 text-lg font-bold text-gray-700 md:text-xl">私について</h3>
 
-      <div className="flex flex-col gap-x-8 mx-2 mt-4 md:flex-row md:justify-between md:mx-8 md:mt-8">
-        <div className="flex flex-col justify-between w-full">
+      <div className="flex flex-col-reverse gap-x-8 md:flex-row md:justify-between md:mt-8">
+        <div className="flex flex-col flex-1 justify-between px-4 mt-4 w-full md:mt-0">
           <p className="text-lg prose">
             大阪の鍼灸整骨院で柔道整復師として働いています。職場のHP作成をきっかけにWeb制作、プログラミングの学習をスタートしました。
             <br />
-            次々と学習を進めていくごとに制作することの楽しさ、喜びを知り、誰かの役に立つ、利用してもらえるものを作りたいという気持ちが大きくなり、Web制作、開発を本業とするべく学習しています。
+            次々と学習を進めていくごとに制作することの楽しさ、喜びを知り、誰かの課題を解決できるようなものを作りたいという気持ちが大きくなり、Webプログラミングを本業とするべく学習しています。
+            <br />
+            約一年間、一日も欠かさず学習を続けており、ひとつひとつできることが増えていく喜びを感じています。
+            <br />
+            プログラミング技術向上への貪欲さ、常に情報をキャッチしようとする姿勢は誰にも負けません！
+            よろしくお願いします！
           </p>
 
-          <div className="flex flex-col justify-between items-end mt-4 md:flex-row md:flex-1 md:mt-0">
+          <div className="flex flex-col justify-between items-end mt-8 md:flex-row md:flex-1">
             <ul className="flex gap-x-4 justify-end text-2xl md:text-4xl">
               {icons.map((icon) => (
                 <li key={icon.label}>{icon.icon}</li>
               ))}
-              <li></li>
             </ul>
 
             <div className="mt-4 text-right">
