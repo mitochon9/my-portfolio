d912232ecedeaf636bddba8ca1713b9e2c1915cd
mitochon9 <mitochon9@gmail.com>
2022-01-30 19:31:27 +0900

    💄 fix hero animation

--- src/pages/index/animationHero/index.tsx
+++ src/pages/index/animationHero/index.tsx
@@ -11,13 +11,6 @@ export const AnimationHero: VFC = () => {
   const listContainer: LegacyRef<HTMLDivElement> | undefined = useRef(null);
 
   const listWrapper: LegacyRef<HTMLDivElement> | undefined = useRef(null);
-  const [listWrapperWidth, setListWrapperWidth] = useState(0);
-  useEffect(() => {
-    if (listWrapper.current) {
-      const clientWidth = listWrapper?.current.clientWidth;
-      setListWrapperWidth(clientWidth);
-    }
-  }, []);
 
   const list: LegacyRef<HTMLUListElement> | undefined = useRef(null);
   const [listWidth, setListWidth] = useState(0);
@@ -29,56 +22,54 @@ export const AnimationHero: VFC = () => {
   }, []);
 
   useEffect(() => {
-    ScrollTrigger.refresh();
-    gsap.to(list.current, {
-      x: () => -(listWidth - listWrapperWidth),
-      ease: "none",
+    const listTl = gsap.timeline({
       scrollTrigger: {
         trigger: listContainer.current,
         start: "top top",
-        end: () => `+=${listWidth - listWrapperWidth}`,
-        scrub: true,
         pin: true,
+        end: `+=${innerHeight}`,
+        scrub: 0.5,
         pinSpacing: false,
+        // anticipatePin: 1,
+        // invalidateOnRefresh: true,
       },
     });
-  }, [listWidth, listWrapperWidth]);
+
+    listTl.to(list.current, {
+      delay: 0.1,
+      x: -listWidth + innerWidth - 120,
+      ease: "none",
+    });
+
+    ScrollTrigger.refresh();
+    // listTl.scrollTrigger.kill();
+  }, [listWidth]);
 
   return (
     <div className="pb-48 -mx-2 bg-white border border-white lg:mt-[86px] xl:mx-0">
       <FirstView />
 
-      <div ref={listContainer} className="flex justify-center items-center">
-        <div ref={listWrapper} className="relative w-full h-[420vw]">
-          <ul ref={list} className="flex absolute top-[60px] left-0">
-            <li className="flex justify-center items-center w-[90vw] h-[100vh] text-[32px] font-bold">
-              <SiHtml5 className="text-[160px] text-[#e34f26] md:text-[200px] lg:text-[240px]" />
+      <div ref={listContainer} className="flex items-center min-h-[100vh]">
+        <div ref={listWrapper} className="overflow-hidden px-12 max-w-full">
+          <ul ref={list} className="inline-flex">
+            <li>
+              <SiHtml5 className="w-screen text-[160px] text-[#e34f26] md:text-[200px] lg:text-[240px]" />
             </li>
-            <li className="flex justify-center items-center w-[90vw] h-[100vh] text-[32px] font-bold">
-              <SiCss3 className="text-[160px] text-[#2965f1] md:text-[200px] lg:text-[240px]" />
+            <li>
+              <SiCss3 className="w-screen text-[160px] text-[#2965f1] md:text-[200px] lg:text-[240px]" />
             </li>
-            <li className="flex justify-center items-center w-[90vw] h-[100vh] text-[32px] font-bold">
-              <SiTypescript className="text-[160px] text-[#3178c6] md:text-[200px] lg:text-[240px]" />
+            <li>
+              <SiTypescript className="w-screen text-[160px] text-[#3178c6] md:text-[200px] lg:text-[240px]" />
             </li>
-            <li className="flex justify-center items-center w-[90vw] h-[100vh] text-[32px] font-bold">
-              <SiReact className="text-[160px] text-[#00d8ff] md:text-[200px] lg:text-[240px]" />
+            <li>
+              <SiReact className="w-screen text-[160px] text-[#00d8ff] md:text-[200px] lg:text-[240px]" />
             </li>
-            <li className="flex justify-center items-center w-[90vw] h-[100vh] text-[32px] font-bold">
-              <SiNextdotjs className="text-[160px] md:text-[200px] lg:text-[240px]" />
+            <li>
+              <SiNextdotjs className="w-screen text-[160px] md:text-[200px] lg:text-[240px]" />
             </li>
           </ul>
         </div>
       </div>
-
-      {/* <HtmlAnimation />
-
-<CssAnimation />
-
-<TypeScriptAnimation />
-
-<ReactAnimation />
-
-<NextJsAnimation /> */}
 
       <TypewriterAnimation />
     </div>
